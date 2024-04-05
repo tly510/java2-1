a814c94ffa722709abc6af9171bcd0078b1e055e
tly510 <ehdgy510@gmail.com>
2024-04-05 11:27:13 +0900

    24.4.5 수업

--- Chapter21.java
+++ Chapter21.java
@@ -1,7 +1,7 @@
 public class Chapter21 {
     public static void main(String[] args) {
-        public static int sum(int n, int m) {
-            s = sum(i+10);
+        var foo = 200;
+        var name;
         }
     }
 }
